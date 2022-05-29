af6e3de62eab2f035d7cad7625b89df7bc4d044a
Reid Stidolph <rstidolph@juniper.net>
2022-05-29 08:40:47 -0700

    cleanup, moved sausage making to helpers

--- app/devices/controller.js
+++ app/devices/controller.js
@@ -1,104 +1,32 @@
 'use strict'
 const helpers = require('./helpers')
 
-// function to transform device input
-function processNewDevice(rawDeviceInput) {
-
-  // instantiate new device object
-  let device = {
-    ...{raw: rawDeviceInput},
-    ...rawDeviceInput
-  }
-
-  // instantiate a networks array 
-  device.networks = {}
-
-  // iterate through network objects from input
-  rawDeviceInput.networks.forEach((network)=>{
-    // regex for extracting PCI bus numbers
-    const pciBusRegex = /(?<busId>\d{4}:\d{2}:\d{2}\.\d)/
-    const usbBusRegex = /usb@\d+:\d+/
-
-    // skip if no pci bus info
-    if (!network.businfo) {
-      return
-    }
-
-    // instantiate a network object for saving
-    let outputNetwork = {}
-    let pciBusTestResults = pciBusRegex.exec(network.businfo)
-    let usbBusTestResults = usbBusRegex.test(network.businfo)
-
-    // process network device
-    if (!pciBusTestResults && !usbBusTestResults) {
-
-      // skip and log if businfo is not PCI or USB
-      console.log(`ERROR: got unknown PCI businfo: ${network.businfo}`)
-      return
-
-    } else if (pciBusTestResults) {
-
-      // got a PCI ethernet device
-      // create ethernet array if it doesn't exist
-      if (!device.networks.ethernet) {
-        device.networks.ethernet = []
-      }
-      // set additional network object details
-      outputNetwork.pciAddress = pciBusTestResults.groups.busId
-      outputNetwork.product = network.product
-      outputNetwork.vendor = network.vendor
-      device.networks.ethernet.push(outputNetwork)
-      
-      return
-
-    } else if (usbBusTestResults) {
-      
-      // got a usb device
-      // see if LTE
-      if(network.configuration && network.configuration.driver == 'qmi_wwan') {
-        // got a LTE device
-        // create lte array if it doesn't exist
-        if (!device.networks.lte) {
-          device.networks.lte = []
-        }
-        // set additional network object details
-        outputNetwork.targetInterface = network.logicalname
-        outputNetwork.product = network.product
-        outputNetwork.vendor = network.vendor
-        device.networks.lte.push(outputNetwork)
-      }
-    }
-  })
-
-  // sort networks by PCI addr
-  device.networks.ethernet.sort(helpers.pciCompare)
-
-  // construct device key
-  device.key = `${device.baseboardManufacturer}_${device.baseboardProductName}_${device.baseboardSerialNumber}_${device.chassisManufacturer}_${device.chassisSerialNumber}_${device.systemManufacturer}_${device.systemProductName}_${device.systemSerialNumber}`
-
-  return device
-}
-
 // export module
 module.exports = {
+  // get devices from DB
 	async getAll(req, res) {
 		try {
-      const devices = await helpers.getAll({})
 
+      const devices = await helpers.getAll({})
       return res.send(devices)
+
     } catch (error) {
+
       return res.status(400).send({
         status: 'failure'
       })
+
     }
   },
+  
+  // add a new device
   async addNew(req, res) {
-
-    //console.log(req.body)
-
     try {
-      await helpers.addNew(processNewDevice(req.body));
 
+      // process and transform device info
+      let transformedDevice = helpers.processNewDevice(req.body)
+      // add to DB
+      await helpers.addNew(transformedDevice)
       return res.send('Device info received.')
 
     } catch (error) {
@@ -106,6 +34,7 @@ module.exports = {
       return res.status(400).send({
         status: 'failure'
       })
+
     }
   }
 }
--- app/devices/helpers.js
+++ app/devices/helpers.js
@@ -6,10 +6,12 @@ const mongoose = require('mongoose')
 const Devices = mongoose.model('Devices')
 
 module.exports = {
+
   // fetch all devices from DB
   async getAll() {
     return await Devices.find({})
   },
+
   // add new device to DB
   async addNew(device) {
     // dup check
@@ -24,8 +26,86 @@ module.exports = {
       return await new Devices(device).save()
     }
   },
-  // compare function for sorting by PCI addresses
-  pciCompare(a, b) {
-    return a.pciAddress.localeCompare(b.pciAddress)
+
+  // function to transform device input
+  processNewDevice(rawDeviceInput) {
+
+    // instantiate new device object
+    let device = {
+      ...{raw: rawDeviceInput},
+      ...rawDeviceInput
+    }
+
+    // clear the networks for re-write
+    device.networks = {}
+
+    // iterate through network objects from input
+    rawDeviceInput.networks.forEach((network)=>{
+
+      // regex for extracting PCI and USB bus numbers
+      const pciBusRegex = /(?<busId>\d{4}:\d{2}:\d{2}\.\d)/
+      const usbBusRegex = /usb@\d+:\d+/
+
+      // skip if no pci bus info
+      if (!network.businfo) {
+        return
+      }
+
+      // instantiate a network object for saving
+      let outputNetwork = {}
+      // test to see if PCI or USB device
+      let pciBusTestResults = pciBusRegex.exec(network.businfo)
+      let usbBusTestResults = usbBusRegex.test(network.businfo)
+
+      // process network device
+      if (!pciBusTestResults && !usbBusTestResults) {
+
+        // skip and log if businfo is not PCI or USB
+        console.log(`ERROR: got unknown PCI businfo: ${network.businfo}`)
+        return
+
+      } else if (pciBusTestResults) {
+
+        // got a PCI ethernet device
+        // create ethernet array if it doesn't exist
+        if (!device.networks.ethernet) {
+          device.networks.ethernet = []
+        }
+        // set additional network object details
+        outputNetwork.pciAddress = pciBusTestResults.groups.busId
+        outputNetwork.product = network.product
+        outputNetwork.vendor = network.vendor
+        device.networks.ethernet.push(outputNetwork)
+        
+        return
+
+      } else if (usbBusTestResults) {
+        
+        // got a usb device
+        // see if LTE
+        if(network.configuration && network.configuration.driver == 'qmi_wwan') {
+          // got a LTE device
+          // create lte array if it doesn't exist
+          if (!device.networks.lte) {
+            device.networks.lte = []
+          }
+          // set additional network object details
+          outputNetwork.targetInterface = network.logicalname
+          outputNetwork.product = network.product
+          outputNetwork.vendor = network.vendor
+          device.networks.lte.push(outputNetwork)
+        }
+      }
+    })
+
+    // sort networks by PCI addr
+    device.networks.ethernet.sort((a, b) => {
+      return a.pciAddress.localeCompare(b.pciAddress)
+    })
+
+    // construct device key
+    device.key = `${device.baseboardManufacturer}_${device.baseboardProductName}_${device.baseboardSerialNumber}_${device.chassisManufacturer}_${device.chassisSerialNumber}_${device.systemManufacturer}_${device.systemProductName}_${device.systemSerialNumber}`
+
+    return device
   }
 }
